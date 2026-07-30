af0b9454e831be02da9f099c61ce1387e5d1f279
Ermite-crypto <hachami16090@gmail.com>
2026-07-30 12:45:54 +0100

    Add service worker

---
+++ app/static/service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='moharrir-agents-v2';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
